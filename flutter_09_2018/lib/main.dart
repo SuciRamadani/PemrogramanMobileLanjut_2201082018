1bdaff5eed066b316db8455aa2779d75ee43d217
SuciRamadani <126840716+SuciRamadani@users.noreply.github.com>
2023-10-24 10:44:50 +0700

    flutter video 9 image widget

---
+++ flutter_09_2018/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Image Widget"),
+        ),
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 500,
+            color: Colors.black,
+            child: Image(
+              fit: BoxFit.contain,
+              image: NetworkImage("https://picsum.photos/200"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
