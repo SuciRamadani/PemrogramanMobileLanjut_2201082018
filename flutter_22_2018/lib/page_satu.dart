8e7cd9e2e2ea02ee7f6860964a31c2f0ca36dd30
SuciRamadani <126840716+SuciRamadani@users.noreply.github.com>
2023-10-28 14:41:04 +0700

    flutter video 22 Navigation

---
+++ flutter_22_2018/lib/page_satu.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import './page_dua.dart';
+
+class pageSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page Satu"),
+      ),
+      body: Center(
+        child: Text(
+          "INI PAGE 1",
+          style: TextStyle(
+            fontSize: 50,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) {
+                return pageDua();
+              },
+            ),
+          );
+        },
+        child: Icon(Icons.keyboard_arrow_right),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
